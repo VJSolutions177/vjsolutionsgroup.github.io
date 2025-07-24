9623b530cb04ab6bb4c52504b9b05916aa358650
VJSolutions177 <info@vjsolutionsgroup.com>
2025-07-24 17:25:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Dark Mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+
+// Slider Logic
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach(slide => slide.style.display = "none");
+  slides[index].style.display = "block";
+}
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 3000);
+
+window.onload = () => showSlide(currentSlide);
 AOS.init({
   duration: 1200,
   once: true
