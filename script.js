a355286bc975f24ebb5c4a2396ff713976dfbe77
VJSolutions177 <info@vjsolutionsgroup.com>
2025-07-24 18:19:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// JS content goes here 
+// Mobile Navbar Toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
